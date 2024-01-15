f0eb5db060b00d25b658070d7b1dc4b97473b6ca
fletcherjacob <fletcher.c.jacob@gmail.com>
2024-01-15 18:32:13 -0500

    finish preprocessing script to handle large files

--- runtime/preprocessing.py
+++ runtime/preprocessing.py
@@ -1,4 +1,3 @@
-import boto3
 import logging
 from pyspark.sql import SparkSession, DataFrame
 from pyspark.sql.types import IntegerType, StringType
@@ -6,21 +5,40 @@ from pyspark.sql import functions as F
 from pyspark.sql.functions import col, from_unixtime, date_trunc, udf, lit
 
 
-def init_logging():
-    logging.basicConfig(level=logging.INFO)
-    logger = logging.getLogger(__name__)
-    logger.info("Logging initialized.")
-
 
 class SparkData:
-    def __init__(self, data):
-        self.sdf = data
+    def __init__(self, path, preproccessing_job= True):
+        self.path = path
+        self.init_logging()
+        self.sdf = None
+        self.create_spark_session()
+        try:
+            if self.path.endswith(".json"):
+                self.sdf = self.spark.read.json(path)
+
+            if self.path.endswith(".csv"):
+                self.sdf = self.spark.read.csv(path, header = True)
+        except Exception as e:
+            print(e)
+            logging.error("Make Sure File is a valid format('.csv' or '.json')")
+            exit()
+
+        if preproccessing_job:
+            self.preproccessing_run()
+
         self.unique_users = None
         self.unique_users_count = None
 
+    
+    def init_logging(self):
+        logging.basicConfig(level=logging.INFO)
+        logger = logging.getLogger(__name__)
+        logger.info("Logging initialized.")
+
+
     def clean_sdf(self):
-        self.columns_to_keep = ['userId','artist','ts','page','level','location']
-        self.sdf = self.sdf.select(self.columns_to_keep).dropna(subset= 'userId')
+        self.columns_to_keep = ["userId", "artist", "ts", "page", "level", "location"]
+        self.sdf = self.sdf.select(self.columns_to_keep).dropna(subset="userId")
         self.unique_users = self.sdf.select("userId").distinct()
         self.unique_users_count = self.unique_users.count()
         return self
@@ -134,10 +152,21 @@ class SparkData:
                 f"Missing Users {feature_name}: {self.unique_users_count - sdf_user_count}"
             )
             return sdf
-        
+
+    def build_labels(self):
+        self.labels = (
+            self.sdf[["userId", "page"]]
+            .filter(col("page").isin(["Cancellation Confirmation"]))
+            .distinct()
+        )
+        self.labels = self.labels.withColumn("label", lit(1))
+        self.labels = self.labels.drop("page")
 
 
-        
+        self.labels = self.handle_missing_users(self.labels,"label")
+
+
+        return self
 
     def build_features(self):
         self.build_song_counts()
@@ -145,43 +174,79 @@ class SparkData:
         self.build_positive_usage()
         self.build_negative_usage()
         self.build_user_unique_locations()
+        self.build_labels()
+
+        dfs = [
+            self.song_counts,
+            # self.song_listened_mean,
+            # self.average_daily_listens,
+            self.user_level,
+            self.positive_usage,
+            self.neg_interactions,
+            self.unique_locations,
+            # self.distinct_artist,
+            # self.page_count,
+        ]
+
+        self.processed_sdf = self.labels
+
+        for df in dfs:
+            self.processed_sdf = self.processed_sdf.join(df, "userId", "outer")
+            del df
+
+        return self
 
 
 
-def main():
-    init_logging()
+        num_rows = self.processed_sdf.count()
+        num_columns = len(self.processed_sdf.columns)
+        logging.info(f"Shape: ({num_rows}, {num_columns})")
+        logging.info(f"Total samples: {num_rows}")
+        logging.info(f"Pos Labels: {self.processed_sdf[['label']].groupBy().sum().collect()[0]['sum(label)']}")
+
+    def export_file(self, output_path, file_type):
+        if file_type  == "csv":
+            self.processed_sdf.coalesce(1).write.csv(output_path)
+        if file_type == 'json':
+            self.processed_sdf.coalesce(1).write.json(output_path)
+        else:
+            logging.info(f"Please select eithe json or csv as filetype {str(file_type)} is not a suported format")
+
+    def create_spark_session(self,app_name="Sparkify", default_settings = True ,  total_physical_cores=16,driver_memory = 8,executor_memory = 8):
+    # Calculate available cores for Spark
+        if default_settings == False:
+            total_physical_cores = input(" Available Cores")
+            driver_memory =  input(" Driver Memory Allowance")
+            executor_memory = input("Executor Memory Allowance")
+
+        available_cores_for_spark =int( total_physical_cores - 2)
+        # Configure Spark session
+        self.spark = (
+            SparkSession.builder.appName(app_name)
+            .config("spark.driver.memory", str(int(driver_memory)) + "g")
+            .config("spark.executor.memory", str(int(executor_memory)) + "g")
+            .config("spark.executor.cores", available_cores_for_spark)
+            .getOrCreate()
+        )
+
+        return self
     
-    try:
-        spark.stop()
-    except NameError:
-        pass
+    def spark_stop(self):
+         self.spark.stop()
 
-    # Create a Spark session with an appropriate app name and executor cores
-    total_physical_cores = 16
-    available_cores_for_spark = total_physical_cores - 2
-
-    spark = (
-        SparkSession.builder.appName("Sparkify")
-        .config("spark.driver.memory", "12g")
-        .config("spark.executor.memory", "12g")
-        .config("spark.executor.cores", available_cores_for_spark)
-        .getOrCreate()
-    )
-
-    # Load Data To Spark Dataframe
-    try:
-        #path = input("Provide location of json data:")
-        path="/Users/jacobfletcher/git/churn_project/data/lg_sparkify_event_data.json"
-        sdf = spark.read.json(path)
-    except Exception as e:
-        logging.error(f"Error reading JSON file: {e}")
-
-    spark_data = SparkData(sdf)
-    spark_data.clean_sdf()
-    spark_data.build_features()
-
-    spark.stop()
-
-
+    def preproccessing_run(self):
+        self.clean_sdf()
+        self.build_features()
+        output_path = input("File Outpu Path:")
+        file_type = input("File Type:")
+        self.export_file(
+            output_path=output_path,
+            file_type=file_type
+        )
+    
 if __name__ == "__main__":
-    main()
+    # Instantiate the class and run preprocessing
+    path = input("Filepath: ")
+    spark_data = SparkData(path=path)
+    
+ 
